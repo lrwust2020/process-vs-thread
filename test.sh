40c25a245caf3ca950254d886289795858bc5bb6
shenbowei <shenbowei0@gmail.com>
2017-03-22 16:52:49 +0800

    增加文本输入

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 make
-echo -e "MultiProcess process=800 count=5000 output=测试程序" >>result
-{ time ./MultiProcess 800 5000 测试程序;} 2>>result
-echo -e "MultiThread thread=800 count=5000 output=测试程序" >>result
-{ time ./MultiThread 800 5000 测试程序;} 2>>result
+echo -e "MultiProcess process=1000 count=500 output=1KB" >>result
+{ time ./MultiProcess 1000 500 $(< 1KB);} 2>>result
+echo -e "MultiThread thread=1000 count=500 output=1KB" >>result
+{ time ./MultiThread 1000 500 $(< 1KB);} 2>>result
